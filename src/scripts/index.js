6e5e429b6bed805111984e208e3b66cf9af43d3d
John Dalton <johndalton95@yahoo.com.br>
2023-09-22 20:41:30 -0300

    Adicionado novo arquivo css de variáveis e javascript de eventos. Agora exibe os 10 últimos eventos de Push e Create do usuário. Adicionado estilização para usuário escolher a cor do tema.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,45 +1,75 @@
-import { getUser } from './services/user.js'
-import { getRepositories } from './services/repositories.js'
+import { getUser } from "./services/user.js";
+import { getRepositories } from "./services/repositories.js";
+import { getEvents } from "./services/events.js";
 
-import { user } from './objects/user.js'
-import { screen } from './objects/screen.js'
+import { user } from "./objects/user.js";
+import { screen } from "./objects/screen.js";
 
 document.getElementById("btn-search").addEventListener("click", () => {
   const userName = document.getElementById("input-search").value;
-  if(validateEmptyInput(userName)) return
+  if (validateEmptyInput(userName)) return;
   getUserData(userName);
 });
+
+let themeButtons = document.getElementsByClassName("btn-theme");
+
+Array.from(themeButtons).forEach((btn) => {
+  btn.addEventListener("click", btnTheme);
+});
+
+function btnTheme(e) {
+  let btn = e.target.classList[1];
+  const colorTheme = document.querySelector(":root");
+
+  switch (btn) {
+    case "hotpink":
+      colorTheme.style.setProperty("--theme-color", btn);
+      break;
+
+    case "red":
+      colorTheme.style.setProperty("--theme-color", btn);
+      break;
+    case "blue":
+      colorTheme.style.setProperty("--theme-color", btn);
+      break;
+    case "cyan":
+      colorTheme.style.setProperty("--theme-color", btn);
+      break;
+  }
+}
 
 document.getElementById("input-search").addEventListener("keyup", (e) => {
   const userName = e.target.value;
   const key = e.which || e.keyCode;
   const isEnterKeyPressed = key === 13;
-  
+
   if (isEnterKeyPressed) {
-    if(validateEmptyInput(userName)) return
+    if (validateEmptyInput(userName)) return;
     getUserData(userName);
   }
 });
 
-function validateEmptyInput(userName){
-  if(userName.length === 0){
-    alert('Preencha o campo com o nome do usuário do GitHub.')
-    return true
+function validateEmptyInput(userName) {
+  if (userName.length === 0) {
+    alert("Preencha o campo com o nome do usuário do GitHub.");
+    return true;
   }
 }
 
 async function getUserData(userName) {
-  const userResponse = await getUser(userName)
-  
-  if(userResponse.message === 'Not Found'){
-    screen.renderNotFound()
-    return
-  }
-  
-  const repositoriesResponse = await getRepositories(userName)
+  const userResponse = await getUser(userName);
 
-  user.setInfo(userResponse)
-  user.setRepositories(repositoriesResponse)
-  
-  screen.renderUser(user)
+  const eventsResponse = await getEvents(userName);
+
+  if (userResponse.message === "Not Found") {
+    screen.renderNotFound();
+    return;
+  }
+
+  const repositoriesResponse = await getRepositories(userName);
+
+  user.setInfo(userResponse);
+  user.setRepositories(repositoriesResponse);
+  user.setEvents(eventsResponse);
+  screen.renderUser(user);
 }
--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,31 +1,77 @@
 const screen = {
-    userProfile: document.querySelector(".profile-data"),
-    renderUser(user){
-        this.userProfile.innerHTML = 
-            `<div class="info">
+  userProfile: document.querySelector(".profile-data"),
+  renderUser(user) {
+    let eventsUser = "";
+
+    user.events.forEach((event) => {
+      if (event.type === "PushEvent") {
+        event.payload.commits.forEach((commit) => {
+          eventsUser += `<li><p>${event.repo.name}<br><span class="commits">"${
+            commit.message ?? "Sem commits para esse evento 😥"
+          }"</span></p></li>`;
+        });
+      } else if (event.type === "CreateEvent") {
+        eventsUser += `<li><p>${event.repo.name}<br><span class="commits">"${
+          event.payload.description ?? "Sem commits para esse evento😥"
+        }"</span></p></li>`;
+      } else {
+        eventsUser += `<li><p>Nenhum Evento encontrado 😥</p></li>`;
+      }
+    });
+    this.userProfile.innerHTML = `<div class="info">
                 <img src="${user.avatarUrl}" alt="Foto do perfil do usuário"/>
                 <div class="data">
                     <h1>${user.name ?? "Não possui nome cadastrado 😢"}</h1>
                     <p>${user.bio ?? "Não possui bio cadastrada 😢"}</p>
-                    <p>Seguidores: ${user.followers ?? "Não possui seguidores 😥"}</p>
-                    <p>Seguindo: ${user.following ?? "Não segue ninguém no Github 😥"}</p>
+                    <p>👥 Seguidores: ${
+                      user.followers ?? "Não possui seguidores 😥"
+                    }</p>
+                    <p>👥 Seguindo: ${
+                      user.following ?? "Não segue ninguém no Github 😥"
+                    }</p>
                 </div>
             </div>`;
 
-        let repositoriesItens = ''
-        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}"
-        target="_blank">${repo.name}</a></li>`)
+    let repositoriesItens = "";
+    user.repositories.forEach(
+      (repo) =>
+        (repositoriesItens += `<li><a href="${repo.html_url}"
+        target="_blank">${repo.name}<br><span>🍴${repo.forks}</span><span>⭐${
+          repo.stargazers_count
+        }</span><span>👀${repo.watchers}</span><span>💻${
+          repo.language ?? "Nenhuma"
+        }</span></a></li>`)
+    );
 
-        if(user.repositories.length > 0){
-            this.userProfile.innerHTML += `<div class="repositories section">
-                                            <h2>Repositórios</h2>
-                                            <ul>${repositoriesItens}</ul>
-                                          </div>`
-        }
-    },
-    renderNotFound(){
-        this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
+    if (user.repositories.length > 0) {
+      this.userProfile.innerHTML += `<div class="repositories section">
+                                                <h2>Repositórios</h2>
+                                                <ul>${repositoriesItens}</ul>
+                                            </div>`;
     }
-}
+    if (user.events.length > 0) {
+      this.userProfile.innerHTML += `<div class="events">
+                                                <h2>Eventos</h2>
+                                                <ul>${
+                                                  eventsUser ??
+                                                  "Nenhum evento encontrado 😥"
+                                                }</ul>
+                                            </div>`;
+    } else {
+      this.userProfile.innerHTML += `<div class="events">
+                                                <h2>Eventos</h2>
+                                                <ul><li>'Nenhum evento encontrado 😥'</li></ul>
+                                            </div>`;
+    }
+  },
+  renderNotFound() {
+    this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>";
+  },
+};
 
-export { screen }
+export { screen };
+
+//repositories[0].forks
+//repositories[0].stargazers_count
+//repositories[0].watchers
+//language
